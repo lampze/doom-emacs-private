28fc61ced19c5cad033ec43f2d99e6dd252030c4
shirui <1229408499@qq.com>
2018-03-02 12:20:06 +0800

    删除无用的模块

--- init.el
+++ init.el
@@ -26,7 +26,8 @@
        workspaces        ; tab emulation, persistence & separate workspaces
 
        :completion
-       company           ; the ultimate code completion backend
+       (company          ; the ultimate code completion backend
+        +auto)
        ivy               ; a search engine for love and life
       ;helm              ; the *other* search engine for love and life
       ;ido               ; the other *other* search engine...
@@ -48,41 +49,41 @@
        dired             ; making dired pretty [functional]
        electric-indent   ; smarter, keyword-based electric-indent
        eshell            ; a consistent, cross-platform shell (WIP)
-       gist              ; interacting with github gists
+      ;gist              ; interacting with github gists
        imenu             ; an imenu sidebar and searchable code index
-       impatient-mode    ; show off code over HTTP
+      ;impatient-mode    ; show off code over HTTP
       ;macos             ; MacOS-specific commands
        make              ; run make tasks from Emacs
        neotree           ; a project drawer, like NERDTree for vim
-       password-store    ; password manager for nerds
+      ;password-store    ; password manager for nerds
        pdf               ; pdf enhancements
        rotate-text       ; cycle region at point between text candidates
        term              ; terminals in Emacs
-       tmux              ; an API for interacting with tmux
+      ;tmux              ; an API for interacting with tmux
        upload            ; map local to remote projects via ssh/ftp
 
        :lang
        assembly          ; assembly for fun or debugging
        cc                ; C/C++/Obj-C madness
-       crystal           ; ruby at the speed of c
-       clojure           ; java with a lisp
+      ;crystal           ; ruby at the speed of c
+      ;clojure           ; java with a lisp
        csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
        elixir            ; erlang done right
        elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ess               ; emacs speaks statistics
-       go                ; the hipster dialect
-       (haskell +intero) ; a language that's lazier than I am
+      ;go                ; the hipster dialect
+      ;(haskell +intero) ; a language that's lazier than I am
        hy                ; readability of scheme w/ speed of python
        (java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        julia             ; a better, faster MATLAB
        latex             ; writing papers in Emacs has never been so fun
        ledger            ; an accounting system in Emacs
-       lua               ; one-based indices? one-based indices
+      ;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
-       ocaml             ; an objective camel
+      ;ocaml             ; an objective camel
        ;; (org              ; organize your plain life in plain text
        ;;  +attach          ; custom attachment system
        ;;  +babel           ; running code in org
@@ -90,18 +91,18 @@
        ;;  +export          ; Exporting org to whatever you want
        ;;  +present         ; Emacs for presentations
        ;;  +publish)        ; Emacs+Org as a static site generator
-       perl              ; write code no one else can comprehend
-       php               ; perl's insecure younger brother
+      ;perl              ; write code no one else can comprehend
+      ;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
        rest              ; Emacs as a REST client
-       ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       scala             ; java, but good
+      ;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+      ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+      ;scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
-       swift             ; who asked for emoji variables?
-       typescript        ; javascript, but better
+      ;swift             ; who asked for emoji variables?
+      ;typescript        ; javascript, but better
        web               ; the tubes
 
        ;; Applications are complex and opinionated modules that transform Emacs
