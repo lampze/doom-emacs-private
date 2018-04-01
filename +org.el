f06c81969babea74c5ae4ee0e2019e58a790d9ed
shirui <1229408499@qq.com>
2018-04-01 19:23:39 +0800

    bind C-c c to org-capture, isn't the counsel-org-capture

--- +org.el
+++ +org.el
@@ -1,5 +1,5 @@
 ;;;  -*- lexical-binding: t; -*-
 
 (map!
- "C-c c" 'org-capture
- "C-c C" (λ! (require 'org-capture) (call-interactively 'org-capture-goto-target)))
+ "C-c c" (lambda! (org-capture))
+ "C-c C" (lambda! (require 'org-capture) (call-interactively 'org-capture-goto-target)))
