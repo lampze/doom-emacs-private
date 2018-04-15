89f031afdb33007f0489d6f254e01223b0fc128b
shirui <1229408499@qq.com>
2018-04-15 16:03:10 +0800

    加入了org模板

--- +org.el
+++ +org.el
@@ -3,3 +3,25 @@
 (map!
  "C-c c" (lambda! (org-capture))
  "C-c C" (lambda! (require 'org-capture) (call-interactively 'org-capture-goto-target)))
+
+(setq org-capture-templates nil
+      org-default-notes-file "~/org/inbox.org")
+
+(add-to-list 'org-capture-templates
+             '("j" "Journal" entry
+               (file+datetree "~/org/journal.org")
+               "* %U - %^{heading}\n  %?"))
+
+(add-to-list 'org-capture-templates
+             '("i" "Inbox" entry (file "~/org/inbox.org")
+               "* %U - %^{heading} %^g\n %?\n"))
+
+(add-to-list 'org-capture-templates
+             '("n" "Notes" entry (file "~/org/notes/inbox.org")
+               "* %^{heading} %t %^g\n  %?\n"))
+
+(add-to-list 'org-capture-templates
+             '("t" "Todo" entry
+               (file+headline "~/org/task.org" "Tasks")
+               "* TODO %?\n%u\n" :clock-in t :clock-resume t))
+
