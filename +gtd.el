0b1ad209c0013982f510dce3ec43a331823776ff
shirui <1229408499@qq.com>
2018-07-01 20:46:10 +0800

    Update gtd: 改进了capture的模板

--- +gtd.el
+++ +gtd.el
@@ -2,17 +2,24 @@
 
 (map!
  "C-c c" (lambda! (org-capture))
- "C-c C" (lambda! (require 'org-capture) (call-interactively 'org-capture-goto-target)))
+ "C-c C" (lambda! (require 'org-capture) (call-interactively 'org-capture-goto-target))
+ (:after org
+   (:map org-mode-map
+     "C-c C-." 'org-time-stamp-inactive)))
 
 (setq org-capture-templates '(("i" "Inbox" entry
                                (file+headline "~/.gtd/inbox.org" "Inbox")
-                               "* TODO %i%?")
+                               "* TODO %i%?\n%U")
                               ("t" "Task" entry
                                (file+headline "~/.gtd/inbox.org" "Tasks")
-                               "* TODO %i%?")
+                               "* TODO %i%?\n%U")
                               ("s" "Study" entry
                                (file+headline "~/.gtd/inbox.org" "Study")
-                               "* TODO %i%?"))
+                               "* TODO %i%?\n%U")
+                              ("S" "Someday" entry
+                               (file+headline "~/.gtd/someday.org" "Someday")
+                               "* TODO %i%?\n%U"))
+
       org-refile-targets '(("~/.gtd/gtd.org" :maxlevel . 3)
                            ("~/.gtd/someday.org" :level . 1)
                            ("~/.gtd/inbox.org" :level . 1)
