e1c9c891c5812cf94173185a4f913ac8c9b10c09
shirui <1229408499@qq.com>
2018-07-07 10:10:28 +0800

    Optimization 在去掉pretty-code特性后不会打不开emacs

--- +scheme.el
+++ +scheme.el
@@ -1,4 +1,5 @@
 ;;; ~/.doom.d/+scheme.el -*- lexical-binding: t; -*-
 
-(set-pretty-symbols! 'scheme-mode
-  :lambda "lambda")
+(when (featurep! :ui pretty-code)
+  (set-pretty-symbols! 'scheme-mode
+    :lambda "lambda"))
