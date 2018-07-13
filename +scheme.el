fc6386315280c341af77a2318e7942005790f909
shirui <1229408499@qq.com>
2018-07-13 11:35:48 +0800

    Add scheme complete

--- +scheme.el
+++ +scheme.el
@@ -3,3 +3,5 @@
 (when (featurep! :ui pretty-code)
   (set-pretty-symbols! 'scheme-mode
     :lambda "lambda"))
+
+(set-company-backend! 'scheme-mode '(company-scheme company-dabbrev-code))
--- packages.el
+++ packages.el
@@ -2,3 +2,6 @@
 
 (package! paredit)
 (package! multiple-cursors)
+
+(when (featurep! :completion company)
+  (package! company-scheme :recipe (:fetcher github :repo "lampze/company-scheme")))
