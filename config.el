9c4d988dd05128f3ea31b211c5314a61b11b2b6c
shirui <1229408499@qq.com>
2021-01-05 12:07:23 +0800

    feat: change org-page theme

--- config.el
+++ config.el
@@ -147,7 +147,8 @@
   (setq op/repository-directory "~/Develop/sources/own/lampze.github.io/"
         op/repository-org-branch "source"
         op/repository-html-branch "master"
-        op/theme-root-directory "/home/shirui/.emacs.d/.local/straight/repos/org-page/themes"
+        op/theme-root-directory "~/.doom.d/"
+        op/theme 'sr_mdo
         op/load-directory "/home/shirui/.emacs.d/.local/straight/repos/org-page/"
         op/personal-github-link "https://github.com/lampze/"
         op/site-domain "https://lampze.github.io/"))
