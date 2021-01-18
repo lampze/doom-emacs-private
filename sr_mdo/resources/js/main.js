523f368cfba194f44194fdc603516ed1ecb8aa83
shirui <1229408499@qq.com>
2021-01-18 18:46:20 +0800

    feat: add the prism to highlight

--- sr_mdo/resources/js/main.js
+++ sr_mdo/resources/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
   /*******************************************************************
+   * 1. remove all org exported line number spans
+   * 2. add css class "linenums" to code block
+   ******************************************************************/
+  var $lines = $("span.linenr");
+  var $linedBlocks = $lines.parent();
+  $lines.remove();
+  $linedBlocks.each(function (index) {
+    $(this).addClass("line-numbers");
+  });
+
+  /*******************************************************************
    * 1. replace css class "src" and "example" with "prettyprint", for
    *    prettify.js to render
    * 2. replace detail language css class, e.g. "src-scheme" to
@@ -12,31 +23,15 @@ $(document).ready(function () {
     $.each(classes, function (idx, cls) {
       if (cls.substring(0, 4) === "src-") {
         var lang = cls.substring(4);
-        self.removeClass(cls).addClass("lang-" + lang);
-        self.attr("data-lang", lang);
+        self.removeClass(cls).addClass("language-" + lang + " match-braces");
       }
     });
-    self.addClass("prettyprint");
+    self.replaceWith(
+      $(
+        "<pre><" +
+          self[0].outerHTML.replace(/(^<\w+|\w+>$)/g, "code") +
+          "></pre>"
+      )
+    );
   });
-  $("pre.example")
-    .removeClass("example")
-    .addClass("prettyprint")
-    .attr("data-lang", "example");
-
-  /*******************************************************************
-   * 1. remove all org exported line number spans
-   * 2. add css class "linenums" to code block per the description of
-   *    prettify.js
-   ******************************************************************/
-  var $lines = $("span.linenr");
-  var $linedBlocks = $lines.parent();
-  $lines.remove();
-  $linedBlocks.each(function (index) {
-    $(this).addClass("linenums");
-  });
-
-  /*******************************************************************
-   * pretty print all code blocks
-   ******************************************************************/
-  prettyPrint();
 });
